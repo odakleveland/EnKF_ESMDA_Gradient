c2eb8ed2ea393cf796130cf922dd258475269df1
odakleveland <123749808+odakleveland@users.noreply.github.com>
2023-02-22 13:26:54 +0100

    Update SC_ESMDA_G.m

--- SC_ESMDA_G.m
+++ SC_ESMDA_G.m
@@ -13,7 +13,7 @@ theta=0:4:40;                           %takeoff angles
 % vpt=[2300,2350,2400];vst=[1170,1200,1250];rhot=[2146,2192,2234];
 % vpt=[2300,2500,2800];vst=[1170,1270,1440];rhot=[2146,2192,2234];
 
-nr=[30,45,65,80,85,95,110,115]+20;                        %Interface in time step
+nr=[30,45,65,80,85,95,110,115]+20; %Interface in time step
 vpt=[2300,2500,2150,2250,2400,2500,2300,2250,2400];
 vst=[1170,1270,1070,1120,1170,1270,1170,1120,1170];
 rhot=[2146,2192,2135,2110,2169,2192,2146,2110,2169];
@@ -29,8 +29,9 @@ num=1;
 figure('Name','Velocity model')
 simple_model_plotting(time,vp1D,vs1D,rho1D)
 
+
 %% Forward modeling
-[G,d] = lin_zoeppritz(vpt,vst,rhot,theta,nr);
+[G,d,a_alpha,a_beta,a_rho] = lin_zoeppritz(vpt,vst,rhot,theta,nr);
 
 % a = 8e-03;b=9e-03; %snr ca 5
 % a = 4e-03;b=7e-03; %snr ca 7
@@ -41,7 +42,7 @@ d_noise = d + noise;
 
 SNR_d = rms(d)./rms(d-d_noise)
 
-%d_noise = d;
+d_noise = d;
 
 %% L-curve
 I = eye(size(G'*G));
@@ -63,17 +64,34 @@ xlabel('Residual norm ||Gm_{est} - d||_2^2')
 ylabel('Solution norm ||m_{est}||_2^2')
 title('L-curve')
 
+%% Inversion
+r = residual_norm.^2;
+num_alpha = find(r==min(r)); %from L-curve
+[vp_inv,vs_inv,rho_inv,m_inv,m_est] = det_inversion_damped(G,d_noise,nr,vpt,vst,rhot,num_alpha);
+%[vp_inv, vs_inv, rho_inv] = det_inversion(G,d_noise,nr,vpt,vst,rhot);
+
 %% Ricker Wavelet
 t = -t_max/2+dt:dt:t_max/2-dt;  
 centr_frq = 25; 
 
 w = ricker_wavelet_zero_phased(dt,t,centr_frq);
 
-%% Inversion
-r = residual_norm.^2;
-num_alpha = find(r==min(r)); %from L-curve
-[vp_inv,vs_inv,rho_inv,m_inv,m_est] = det_inversion_damped(G,d_noise,nr,vpt,vst,rhot,num_alpha);
-%[vp_inv, vs_inv, rho_inv] = det_inversion(G,d_noise,nr,vpt,vst,rhot);
+%% Create wavelet 
+d_new = d';
+d_conv = zeros(1,length(time)); %endret så det er mulig å plotte mot tid, riktig?
+for kk = 1:length(d)
+    d_conv(kk)=conv(d_new(:,kk),w,'same');
+end
+
+A = [a_alpha,a_beta,a_rho];
+%A*m_est; %passer ikke
+
+%%
+% figure('Renderer', 'painters', 'Position', [50 40 800 500])
+% hold on,title('No noise'),grid on
+% for kk = 1:length(d)
+%     %plot()
+% end
 
 %% Output
 
@@ -131,9 +149,7 @@ mu=zeros(1,I*length(nr));
 
 %Constant prior mean
 for ii=1:length(nr)
-    mup(ii)=2350;
-    mus(ii)=1270;
-    mur(ii)=2150;
+    mup(ii)=2350;mus(ii)=1270;mur(ii)=2150;
 end
 
 %Linear prior mean
@@ -143,15 +159,11 @@ end
 
 %Gather the prior mean in one vector
 for ii=1:length(nr)
-    mu(1+I*(ii-1))=mup(ii);
-    mu(2+I*(ii-1))=mus(ii);
-    mu(3+I*(ii-1))=mur(ii);
+    mu(1+I*(ii-1))=mup(ii);mu(2+I*(ii-1))=mus(ii);mu(3+I*(ii-1))=mur(ii);
 end
 
 Num_parameters=length(mu);      %Number of parameters in total
-Pvari=500;                      %Variance
-Svari=300;
-Rvari=300;
+Pvari=500;Svari=300;Rvari=300;  %variance
 Covariance_matrix=eye(Num_parameters,Num_parameters); 
 for ii=1:length(nr)
     Covariance_matrix(1+I*(ii-1),1+I*(ii-1))=Pvari;
@@ -208,21 +220,15 @@ if num==0;
 end
 
 a=[100/5,100/10,100/15,100/30,100/40]; %inflation coefficient
-Xp=ESMDA_wavelet_linear(X,w,a,obs,R,Num_ensembles,Num_parameters,Na,bvpt,bvst,brhot,theta,time,nr); %endret fra obs til obs1
-%Xp=ESMDA_wavelet(X,w,a,obs,R,Num_ensembles,Num_parameters,Na,bvpt,bvst,brhot,theta,time,nr); %endret fra obs til obs1
+Xp=ESMDA_wavelet_linear(X,w,a,obs,R,Num_ensembles,Num_parameters,Na,bvpt,bvst,brhot,theta,time,nr);
 
 time_toc=toc
-
-
 %% Gradient-based method
 
 I = 3; %number of parameters
 
 Num_parameters=length(nr)*I;      %Number of parameters in total
-%Variance:
-Pvari=500;Svari=300;Rvari=300; %Before: 500, 300, 300
-%Covariance matrix M
-cov_m=eye(Num_parameters); 
+cov_m=eye(Num_parameters); %covariance model
 for ii=1:length(nr)
     cov_m(1+I*(ii-1),1+I*(ii-1))=Pvari;
     cov_m(2+I*(ii-1),2+I*(ii-1))=Svari;
@@ -233,33 +239,14 @@ for ii=1:length(nr)
     cov_m(3+I*(ii-1),2+I*(ii-1))=0;  cov_m(2+I*(ii-1),3+I*(ii-1))=0; 
 end
 
-% cov_m = zeros(length(m));
-% for i = 1:length(m)
-%    cov_m(i,i) = 1/length(m) * (m(i)-mean(m))*(m(i)-mean(m))'; 
-% end
-%calculate covariance of d
-% cov_d = zeros(length(d));
-% for i = 1:length(d)
-%     cov_d(i,i) = 1/length(d_noise) * (d_noise(i)-mean(d_noise))*(d_noise(i)-mean(d_noise))'; %equation from 
-% end
-
-%same covariance as the EnKF/ES-MDA:
-cov_d = eye(length(d))*max(max(Rd));
-
-%diagonalen gir variansen, bruk plottefunksjon for å fikse 
+cov_d = eye(length(d))*max(max(Rd)); %covariance data
 
 mean_posterior = m_est+cov_m*G'*inv(G*cov_m*G'+cov_d)*(d_noise-G*m_est); % eq 3.37 tarantola
-%m_posterior = cov_m - cov_m*G'*inv(G*cov_m*G'+cov_d)*G*cov_m; %eq 3.38 tarantola
 m_posterior = inv(G'*inv(cov_d)*G+inv(cov_m));
-conf_area = diag(m_posterior);
-conf_area_p = conf_area(1:3:end);
-conf_area_s = conf_area(2:3:end);
-conf_area_d = conf_area(3:3:end);
+conf_area = diag(m_posterior);conf_area_p = conf_area(1:3:end);conf_area_s = conf_area(2:3:end);conf_area_d = conf_area(3:3:end);
 
 %% change from contrast to absolute velocities
-mean_posterior_vel = zeros(length(nr),3);
-conf_area_vel_max = zeros(length(nr),3);
-conf_area_vel_min = zeros(length(nr),3);
+mean_posterior_vel = zeros(length(nr),3);conf_area_vel_max = zeros(length(nr),3);conf_area_vel_min = zeros(length(nr),3);
 
 for i = 1:length(nr)
     mean_posterior_vel(i,1:3) = [mean_posterior(3*(i-1)+1)*vpt(i)+vpt(i),mean_posterior(3*(i-1)+2)*vst(i)+vst(i),mean_posterior(3*i)*rhot(i)+rhot(i)];
@@ -267,7 +254,6 @@ end
 
 %create vectors for velocities after inversion
 vp_mean = [vpt(1), mean_posterior_vel(:,1)'];vs_mean = [vst(1), mean_posterior_vel(:,2)'];rho_mean = [rhot(1), mean_posterior_vel(:,3)'];
-
 for i = 1:length(nr)
     conf_area_vel_max(i,1:3) = [conf_area_p(i)*vp_mean(i+1)+vp_mean(i+1),conf_area_s(i)*vs_mean(i+1)+vs_mean(i+1),conf_area_d(i)*rho_mean(i+1)+rho_mean(i+1)];
     conf_area_vel_min(i,1:3) = [-conf_area_p(i)*vp_mean(i+1)+vp_mean(i+1),-conf_area_s(i)*vs_mean(i+1)+vs_mean(i+1),-conf_area_d(i)*rho_mean(i+1)+rho_mean(i+1)];
@@ -281,35 +267,16 @@ vp_max = [vpt(1),conf_area_vel_max(:,1)'];vs_max= [vst(1),conf_area_vel_max(:,2)
 [vp1D_min,vs1D_min,rho1D_min] = vel_den_vectors(time,nr,vp_min,vs_min,rho_min);
 [vp1D_max,vs1D_max,rho1D_max] = vel_den_vectors(time,nr,vp_max,vs_max,rho_max);
 
-%% Confidence area (Aster
-
-%Using equation 2.32 Aster Inverse
-%mean_posterior_vel = [vp_mean,vs_mean,rho_mean];
-
-%mean_Xp_vel_max = 1.96*sqrt(mean_posterior_vel)+mean_posterior_vel;
-%mean_Xp_vel_min = -1.96*sqrt(mean_posterior_vel)+mean_posterior_vel;
-
-%vp_max = mean_Xp_vel_max(1:9);
-%vs_max = mean_Xp_vel_max(10:18);
-%rho_max = mean_Xp_vel_max(19:27);
-
-%vp_min = mean_Xp_vel_min(1:9);
-%vs_min = mean_Xp_vel_min(10:18);
-%rho_min = mean_Xp_vel_min(19:27);
-
-%[vp1D_max,vs1D_max,rho1D_max] = vel_den_vectors(time,nr,vp_max,vs_max,rho_max);
-%[vp1D_min,vs1D_min,rho1D_min] = vel_den_vectors(time,nr,vp_min,vs_min,rho_min);
-
 %% Plotting fill:
 
 figure(4)
 plot_nl_wavelet_ESMDA_gradient_fill(X,Xp,vpt,vst,rhot,time,nr,I,1,vp1D_max,vp1D_min,vs1D_max,vs1D_min,rho1D_max,rho1D_min,vp1D,vs1D,rho1D)
 
-% %% Plotting mean:
-% 
-% figure(5)
-% plot_nl_wavelet_ESMDA_gradient(X,Xp,vpt,vst,rhot,time,nr,I,1,vp1D_mean,vs1D_mean,rho1D_mean)
-% 
+%% Plotting mean:
+
+figure(5)
+plot_nl_wavelet_ESMDA_gradient(X,Xp,vpt,vst,rhot,time,nr,I,1,vp1D_mean,vs1D_mean,rho1D_mean)
+
 % %% Plot gradient based method:
 % 
 % figure(6)
